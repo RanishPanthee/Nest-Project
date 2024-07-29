d7952f8e36613c7a7689a490eca8669edb7ef547
RanishPanthee <ranish023@gmail.com>
2024-07-29 10:19:51 +0545

    decorators changed

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,14 +1,10 @@
-import { IsEmail, IsString, Matches, MinLength } from "class-validator";
+import { IsEmail, IsString, IsStrongPassword, Matches, MinLength } from "class-validator";
 
 export class LoginDto{
     @IsEmail({}, { message: 'Enter a valid email address' })
     email: string;
 
     @IsString()
-    @MinLength(8, { message: 'Password must be at least 8 characters long' })
-    @Matches(/[a-z]/, { message: 'Password must contain at least one lowercase letter' })
-    @Matches(/[A-Z]/, { message: 'Password must contain at least one uppercase letter' })
-    @Matches(/[0-9]/, { message: 'Password must contain at least one number' })
-    @Matches(/[!@#$%^&*]/, { message: 'Password must contain at least one special character' })
+    @IsStrongPassword()
     password: string;
 }
